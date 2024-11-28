44c473966c7a7d8d172ed7918c98cf961c24ccd0
Kegan Dougal <7190048+kegsay@users.noreply.github.com>
2024-11-28 14:51:07 +0000

    Run demo in CI

--- cmd/chaos/main.go
+++ cmd/chaos/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"time"
 
 	"github.com/element-hq/chaos"
 	"github.com/element-hq/chaos/config"
@@ -10,6 +12,7 @@ import (
 
 func main() {
 	flagConfig := flag.String("config", "", "path to the config YAML")
+	flagTimeoutSecs := flag.Int("timeout_secs", 0, "number of seconds to run chaos")
 	flag.Parse()
 	cfg, err := config.OpenFile(*flagConfig)
 	if err != nil {
@@ -18,6 +21,15 @@ func main() {
 
 	// print WS traffic
 	go chaos.PrintTraffic(cfg.WSPort, cfg.Verbose)
+
+	timeoutSecs := *flagTimeoutSecs
+	if timeoutSecs > 0 {
+		log.Printf("Terminating in %ds\n", timeoutSecs)
+		go func() {
+			time.Sleep(time.Duration(timeoutSecs) * time.Second)
+			os.Exit(0)
+		}()
+	}
 
 	if err := chaos.Bootstrap(cfg); err != nil {
 		log.Fatalf("Bootstrap: %s", err)
